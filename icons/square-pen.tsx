ba8009091eb9cfbdcc3f61fc0dadf420ee7a6d1a
pqoqubbw <pqoqubbw@gmail.com>
2025-01-24 19:47:24 +0100

    fix SquarePenIcon

--- icons/square-pen.tsx
+++ icons/square-pen.tsx
@@ -35,17 +35,14 @@ const SquarePenIcon = () => {
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
+        style={{ overflow: 'visible' }}
       >
         <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
         <motion.path
           d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z"
           variants={penVariants}
           animate={controls}
-          transition={{
-            duration: 0.5,
-            repeat: 1,
-            ease: 'easeInOut',
-          }}
+          style={{ transformOrigin: '18.375px 2.625px' }}
         />
       </svg>
     </div>
