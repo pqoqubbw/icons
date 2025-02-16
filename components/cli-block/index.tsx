28f2c1e5ce1def6ca84131327d57a611d4665fbb
pqoqubbw <pqoqubbw@gmail.com>
2025-02-16 04:13:56 +0100

    fix cli-block responsive for copy button v2

--- components/cli-block/index.tsx
+++ components/cli-block/index.tsx
@@ -29,8 +29,8 @@ const CliBlock = ({ icons }: { icons: Icon[] }) => {
       className="w-full max-w-3xl bg-input/50 rounded-lg overflow-hidden cursor-pointer mt-8 border border-input relative"
       onClick={copyToClipboard}
     >
-      <div className="px-3 py-4 font-mono text-sm flex items-center gap-2 overflow-x-auto whitespace-nowrap pr-14">
-        <div className="flex items-center min-w-0 overflow-hidden">
+      <div className="px-3 py-4 font-mono text-sm md:max-w-full max-w-[calc(100%-56px)] flex items-center gap-2 overflow-x-auto whitespace-nowrap pr-14">
+        <div className="flex items-center min-w-0">
           <span className="shrink-0 mr-2">npx</span>{' '}
           <span className="text-muted-foreground shrink-0">
             shadcn@latest add &quot;https://icons.pqoqubbw.dev/c/
@@ -56,7 +56,7 @@ const CliBlock = ({ icons }: { icons: Icon[] }) => {
           </TextLoop>
         </div>
       </div>
-      <div className="absolute right-3 top-1/2 -translate-y-1/2">
+      <div className="absolute md:right-3 right-0 md:px-0 px-3 top-1/2 -translate-y-1/2 md:bg-transparent bg-[#1D1D1D]">
         <AnimatePresence mode="wait" initial={false}>
           {copied ? (
             <motion.div
