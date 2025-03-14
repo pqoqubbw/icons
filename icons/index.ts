273b4967682f81ac991af0ede38b1250c0d2a0cb
PedroAquino <veartainc@gmail.com>
2025-03-14 10:42:38 -0300

    feat: added hand-heart icon

--- icons/index.ts
+++ icons/index.ts
@@ -214,6 +214,7 @@ import { SquareChevronRightIcon } from '@/icons/square-chevron-right';
 import { SquareChevronLeftIcon } from '@/icons/square-chevron-left';
 import { GalleryHorizontalEndIcon } from '@/icons/gallery-horizontal-end';
 import { GalleryVerticalEndIcon } from '@/icons/gallery-vertical-end';
+import { HandHeartIcon } from '@/icons/hand-heart';
 
 type IconListItem = {
   name: string;
@@ -2116,6 +2117,11 @@ const ICON_LIST: IconListItem[] = [
       'backup',
       'time machine',
     ],
+  },
+  {
+    name: 'hand-heart',
+    icon: HandHeartIcon,
+    keywords: ['hand', 'heart', 'love', 'affection', 'hug', 'cuddle', 'emotion'],
   },
 ];
 
