79ed012628242c66495f9bb5eb8c91d61571ab1f
pqoqubbw <pqoqubbw@gmail.com>
2025-03-30 15:43:09 +0200

    run gen-cli

--- scripts/registry-components.ts
+++ scripts/registry-components.ts
@@ -1518,4 +1518,10 @@ export const components: ComponentDefinition[] = [
     'registryDependencies': [],
     'dependencies': ['motion'],
   },
+  {
+    'name': 'history',
+    'path': path.join(__dirname, '../icons/history.tsx'),
+    'registryDependencies': [],
+    'dependencies': ['motion'],
+  }
 ];
