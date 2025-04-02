8ec64b4481dde1f02499bddc228a019f1b0c458f
pqoqubbw <pqoqubbw@gmail.com>
2025-04-02 18:55:20 +0200

    update link

--- constants.ts
+++ constants.ts
@@ -3,7 +3,7 @@ const LINK = {
   GITHUB: 'https://github.com/pqoqubbw/icons',
   LUCIDE: 'https://lucide.dev',
   MOTION: 'https://motion.dev',
-  HELPY_UI: 'https://helpy-ui.com',
+  HELPY_UI: 'https://helpy-ui.link/from-icons',
 };
 
 export { LINK };
