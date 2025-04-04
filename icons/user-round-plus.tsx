f0ddd7122e20298e3dfa2bb02eacf6094d09a0f9
Wildan Zulfikar <hey@wzulfikar.com>
2025-04-05 01:18:06 +0300

    Add user-round-plus

--- icons/user-round-plus.tsx
+++ icons/user-round-plus.tsx
@@ -103,17 +103,17 @@ const UserRoundPlusIcon = forwardRef<UserRoundPlusIconHandle, UserRoundPlusIconP
           strokeLinejoin="round"
           initial="normal"
         >
-          <path d="M19.914 11.105A7.298 7.298 0 0 0 20 10a8 8 0 0 0-16 0c0 4.993 5.539 10.193 7.399 11.799a1 1 0 0 0 1.202 0 32 32 0 0 0 .824-.738" />
-          <circle cx="12" cy="10" r="3" />
+          <path d="M2 21a8 8 0 0 1 13.292-6" />
+          <circle cx="10" cy="8" r="5" />
           <motion.path
-            d="M16 18h6"
-            variants={horizontalBarVariants}
+            d="M19 16v6"
+            variants={verticalBarVariants}
             initial="normal"
             animate={controls}
           />
           <motion.path
-            d="M19 15v6"
-            variants={verticalBarVariants}
+            d="M22 19h-6"
+            variants={horizontalBarVariants}
             initial="normal"
             animate={controls}
           />
