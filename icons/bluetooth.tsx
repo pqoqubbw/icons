973b104928b5e77ebb2573eae9e0796c8a12e1ee
KISHORE KUMAR S <77736919+KISHORE-KUMAR-S@users.noreply.github.com>
2025-09-23 22:06:10 +0530

    Update bluetooth.tsx

--- icons/bluetooth.tsx
+++ icons/bluetooth.tsx
@@ -72,10 +72,6 @@ const BluetoothIcon = forwardRef<BluetoothIconHandle, BluetoothIconProps>(
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-bluetooth"
         >
           <motion.path
             d="m7 7 10 10-5 5V2l5 5L7 17"
