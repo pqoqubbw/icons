57c96ed14e28271c9464702f8c3db59c03e8c814
KISHORE KUMAR S <77736919+KISHORE-KUMAR-S@users.noreply.github.com>
2025-09-23 22:12:29 +0530

    Update maximize.tsx

--- icons/maximize.tsx
+++ icons/maximize.tsx
@@ -69,10 +69,6 @@ const MaximizeIcon = forwardRef<MaximizeIconHandle, MaximizeIconProps>(
           viewBox="0 0 24 24"
           fill="none"
           stroke="currentColor"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="lucide lucide-maximize-icon"
         >
           <motion.path
             d="M8 3H5a2 2 0 0 0-2 2v3"
