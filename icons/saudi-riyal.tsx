8df2a57c1451fc9862778bce9728e10ce9a6a4ae
KISHORE KUMAR S <77736919+KISHORE-KUMAR-S@users.noreply.github.com>
2025-10-13 17:14:12 +0530

    Update saudi-riyal.tsx

--- icons/saudi-riyal.tsx
+++ icons/saudi-riyal.tsx
@@ -92,25 +92,25 @@ const SaudiRiyalIcon = forwardRef<SaudiRiyalIconHandle, SaudiRiyalIconProps>(
             d="m20 19.5-5.5 1.2"
             initial="normal"
             animate={controls}
-            variants={saudiRiyalVariants}
+            variants={SAUDI_RIYAL_VARIANTS}
           />
           <motion.path
             d="M14.5 4v11.22a1 1 0 0 0 1.242.97L20 15.2"
             initial="normal"
             animate={controls}
-            variants={saudiRiyalVariants}
+            variants={SAUDI_RIYAL_VARIANTS}
           />
           <motion.path
             d="m2.978 19.351 5.549-1.363A2 2 0 0 0 10 16V2"
             initial="normal"
             animate={controls}
-            variants={saudiRiyalVariants}
+            variants={SAUDI_RIYAL_VARIANTS}
           />
           <motion.path
             d="M20 10 4 13.5"
             initial="normal"
             animate={controls}
-            variants={saudiRiyalVariants}
+            variants={SAUDI_RIYAL_VARIANTS}
           />
         </svg>
       </div>
