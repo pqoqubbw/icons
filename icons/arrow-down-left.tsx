f5c862f0d5cf1c85efeebfaa1ce342a37a8c36ff
KISHORE KUMAR S <77736919+KISHORE-KUMAR-S@users.noreply.github.com>
2025-10-13 15:32:12 +0530

    Update icons/arrow-down-left.tsx
    Co-authored-by: dmytro <71014515+pqoqubbw@users.noreply.github.com>

--- icons/arrow-down-left.tsx
+++ icons/arrow-down-left.tsx
@@ -15,7 +15,7 @@ interface ArrowDownLeftIconProps extends HTMLAttributes<HTMLDivElement> {
   size?: number;
 }
 
-const headVariants: Variants = {
+const HEAD_VARIANTS: Variants = {
   normal: { translateX: 0, translateY: 0 },
   animate: {
     translateX: [0, 3, 0],
