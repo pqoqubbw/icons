412f9f3a70b88bea629664b93c9ed2d593b7dae5
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-12-10 12:47:12 +0530

    Added: Hand, Hand Grab, Hand Fist, Hand Metal, Hand Helping, Heart Handshake

---
+++ icons/hand.tsx
@@ -0,0 +1,95 @@
+'use client';
+
+import type { HTMLAttributes } from 'react';
+import { forwardRef, useCallback, useImperativeHandle, useRef } from 'react';
+import { motion, useAnimation } from 'motion/react';
+
+import { cn } from '@/lib/utils';
+
+export interface HandIconHandle {
+  startAnimation: () => void;
+  stopAnimation: () => void;
+}
+
+interface HandIconProps extends HTMLAttributes<HTMLDivElement> {
+  size?: number;
+}
+
+const HandIcon = forwardRef<HandIconHandle, HandIconProps>(
+  ({ onMouseEnter, onMouseLeave, className, size = 28, ...props }, ref) => {
+    const controls = useAnimation();
+    const isControlledRef = useRef(false);
+
+    useImperativeHandle(ref, () => {
+      isControlledRef.current = true;
+      return {
+        startAnimation: () => controls.start('animate'),
+        stopAnimation: () => controls.start('normal'),
+      };
+    });
+
+    const handleMouseEnter = useCallback(
+      (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!isControlledRef.current) {
+          controls.start('animate');
+        } else {
+          onMouseEnter?.(e);
+        }
+      },
+      [controls, onMouseEnter]
+    );
+
+    const handleMouseLeave = useCallback(
+      (e: React.MouseEvent<HTMLDivElement>) => {
+        if (!isControlledRef.current) {
+          controls.start('normal');
+        } else {
+          onMouseLeave?.(e);
+        }
+      },
+      [controls, onMouseLeave]
+    );
+
+    return (
+      <div
+        className={cn(className)}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        {...props}
+      >
+        <motion.svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={size}
+          height={size}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          variants={{
+            normal: { rotate: 0, originX: '50%', originY: '90%' },
+            animate: {
+              rotate: [0, -15, 10, -5, 0],
+              transition: {
+                duration: 0.8,
+                ease: 'easeInOut',
+              },
+            },
+          }}
+          initial="normal"
+          animate={controls}
+        >
+          <path d="M18 11V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2" />
+          <path d="M14 10V4a2 2 0 0 0-2-2a2 2 0 0 0-2 2v2" />
+          <path d="M10 10.5V6a2 2 0 0 0-2-2a2 2 0 0 0-2 2v8" />
+          <path d="M18 8a2 2 0 1 1 4 0v6a8 8 0 0 1-8 8h-2c-2.8 0-4.5-.86-5.99-2.34l-3.6-3.6a2 2 0 0 1 2.83-2.82L7 15" />
+        </motion.svg>
+      </div>
+    );
+  }
+);
+
+HandIcon.displayName = 'HandIcon';
+
+export { HandIcon };
