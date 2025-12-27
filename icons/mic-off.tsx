0e7a371da52fbee5abc335c4ef6bb39c84cc0cd7
Uzairkazi695 <kaziuzair695@gmail.com>
2025-12-27 14:57:10 +0530

    removed comments

--- icons/mic-off.tsx
+++ icons/mic-off.tsx
@@ -90,17 +90,11 @@ const MicOffIcon = forwardRef<MicOffIconHandle, MicOffIconProps>(
           strokeLinecap="round"
           strokeLinejoin="round"
         >
-          {/* Stand */}
           <path d="M12 19v3" />
-          {/* Partial capsule top */}
           <path d="M15 9.34V5a3 3 0 0 0-5.68-1.33" />
-          {/* Left side of curved base */}
           <path d="M16.95 16.95A7 7 0 0 1 5 12v-2" />
-          {/* Right side fragment */}
           <path d="M18.89 13.23A7 7 0 0 0 19 12v-2" />
-          {/* Bottom of capsule */}
           <path d="M9 9v3a3 3 0 0 0 5.12 2.12" />
-          {/* Diagonal strike-through - animated */}
           <motion.path
             d="m2 2 20 20"
             variants={LINE_VARIANTS}
