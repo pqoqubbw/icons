52378e979675079e6d9e4a594ec6c20c4bf40cfb
dmytro <pqoqubbw@gmail.com>
2026-01-12 12:19:56 +0100

    fix layout

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mb-6 flex w-full items-center justify-center gap-1 font-sans text-secondary text-xs">
+    <footer className="mt-auto mb-6 flex w-full items-center justify-center gap-1 font-sans text-secondary text-xs">
       <span>analytics by</span>
       <a
         className="underline underline-offset-3 transition-[decoration-color] duration-100 focus-within:outline-offset-0 hover:decoration-primary focus-visible:outline-1 focus-visible:outline-primary"
