ea68d0ee943d0804e1eb4942c43a603b5f8dd099
gustavo <149335501+gustavo-lola@users.noreply.github.com>
2026-03-01 18:25:12 -0300

    Update registry-components.ts

--- scripts/registry-components.ts
+++ scripts/registry-components.ts
@@ -2285,8 +2285,14 @@ export const components: ComponentDefinition[] = [
     dependencies: ["motion"],
   },
   {
-    name: "graduation-cap",
-    path: path.join(__dirname, "../icons/graduation-cap.tsx"),
+    name: "send",
+    path: path.join(__dirname, "../icons/send.tsx"),
+    registryDependencies: [],
+    dependencies: ["motion"],
+  },
+  {
+    name: "mailbox",
+    path: path.join(__dirname, "../icons/mailbox.tsx"),
     registryDependencies: [],
     dependencies: ["motion"],
   },
